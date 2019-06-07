d6161e69f87cddfdf8c751c9d2d6549931bbde87
afmkhan <47547127+afmkhan@users.noreply.github.com>
2019-06-07 12:08:26 -0400

    board upload

---
+++ BoardForQueensPuzzle.java
@@ -0,0 +1,167 @@
+/**
+  Represent a square chess board for a queens puzzle
+  of a particular size.
+ */
+
+public class BoardForQueensPuzzle {
+    private int lastRankFilled;  /* the highest-numbered rank
+      that holds a queen. */
+    private int[] filesWithQueens;  /* indexed by rank, giving
+      the file number that a queen occupies in that rank.
+      Interacts with lastRankFilled: entries in filesWithQueens
+      are meaningful only for ranks <= lastRankFilled. Entries
+      at higher indexes hold bit patterns that can be interpreted
+      as integers, as always. But those entries are meaningless.
+      This common thinking was used for the filledElements field
+      in List_inArraySlots exactly the same way.
+      */
+
+
+    /**
+      Construct an empty instance of the specified size
+     */
+    public BoardForQueensPuzzle( int ranks) {
+        filesWithQueens = new int[ranks];
+        lastRankFilled = -1;
+    }
+
+
+    /**
+      @return the size of the board
+     */
+    public int ranks() {
+        return filesWithQueens.length;  
+    }
+
+
+    /**
+      @return the boolean value of the statement
+              "The last-added queen introduced a conflict."
+      @precondition: Before the latest queen was added,
+                     no queen attacked another.
+     */
+    public boolean lastIsNg() {
+        //iterate through filesWithQueens 
+        for( int iRanks = 0; iRanks < lastRankFilled; iRanks++){
+            
+            // check if file of new Q equals a previously populated file
+            if (filesWithQueens[iRanks] == filesWithQueens[lastRankFilled])
+                return true;
+            
+            // check if file of new Q is the diagonal of an existing queen
+            // by checking
+            // if horizontal distance = vertical distance
+            else if (Math.abs( filesWithQueens[iRanks]        
+                               -                            
+                               filesWithQueens[lastRankFilled]
+                              )
+                    == 
+                    lastRankFilled - iRanks
+                    )
+                        return true;          
+        }
+        
+        return false;
+    }
+
+
+    /**
+      @return the boolean value of the statement...
+                "The board is fully and legally populated."
+      precondition: All ranks except possibly the last-filled
+        are populated legally. (That is, considering only the
+        ranks before the last-filled rank, no queen attacks
+        another.
+        This method checks the last-filled rank.
+     */
+    public boolean accept() {
+       
+        if( lastRankFilled == ranks() - 1 //check if fully populated
+            &&                            // AND
+            lastIsNg() == false           // legally populated
+            )
+            return true;
+            
+        else return false;                // return false if fails check
+            
+    }
+
+
+    /**
+      Populate the next rank with a queen in position @file
+     */
+    public void populate( int file) {
+        // update lastRankFilled and set the corresponding file
+        filesWithQueens[++lastRankFilled] = file;
+    }
+
+
+    /**
+      Un-do a populate(), to have no queen in the
+      most-recently-populated rank.
+      @precondition: Some rank(s) have been populated.
+     */
+    public void depopulate() {
+        // update lastRankFilled
+        // actual value in the array does not matter
+        lastRankFilled--;
+    }
+
+
+    // ----- skeletal code below here needs no modification -----
+    private static final String INDENT = "         ";
+    /**
+      @return a string representation of this board
+     */
+    public String toString() {
+        int size = filesWithQueens.length; // just for shorthand
+
+        /* Make a header containing file numbers like...
+               3 ranks, populated through rank 1
+               files
+               0  1  2  3 ...
+               __________ ...
+         */
+
+        // sizes and labels that precede the ranks
+        String representation = INDENT
+          + size + " ranks, "
+          + ( // ?: conditional operator
+                lastRankFilled < 0
+              ? "unpopulated"
+              : "populated through rank " + lastRankFilled
+            )
+          + System.lineSeparator();
+
+        // short-circuit the representationture for a zero-sized board
+        if( size == 0) return representation;
+
+        representation += INDENT + "files" + System.lineSeparator()
+            +  INDENT;
+        String underscores = "";
+
+        // file numbers, each in a 3-column field
+        for( int file = 0; file < size; file++) {
+            representation += String.format("%-3d", file);
+            underscores += "___"; // there's gotta be a better way
+        }
+
+        // underline the header
+        representation+=   System.lineSeparator()
+              + INDENT + underscores + System.lineSeparator();
+
+        // representationture each rank
+        for( int rank = 0; rank < size; rank++){
+            representation += // right-justified rank number:
+                   String.format("rank %2d|", rank);
+            for( int file = 0; file < size; file++)
+                if(    rank > lastRankFilled      // no queen in this rank yet
+                    || filesWithQueens[ rank] != file) // no queen in this file
+                     representation += " _ ";
+                else representation += " Q ";
+            representation += System.lineSe
+parator();
+        }
+        return representation;
+    }
+}
